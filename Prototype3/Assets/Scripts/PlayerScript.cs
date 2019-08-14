c9d888ed723d0e2abd74cf0b1c209a0fd676a8d0
PsvitaNeedsCat <53197859+PsvitaNeedsCat@users.noreply.github.com>
2019-08-14 17:10:32 +1200

    PlayerMouseMovement
    - Player moves to where the mouse clicks

--- Prototype3/Assets/Scripts/PlayerScript.cs
+++ Prototype3/Assets/Scripts/PlayerScript.cs
@@ -9,6 +9,7 @@ public class PlayerScript : MonoBehaviour
     private float speed = 5.0f;
     private float leftRightAxis = 0.0f;
     private float upDownAxis = 0.0f;
+    private bool atTarget = false;
     private Vector3 targetPosition;
 
     private void Awake()
@@ -19,20 +20,51 @@ public class PlayerScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        DebugMovement();
+        // If clicking
+        if (Input.GetMouseButton(0))
+        {
+            SetTargetPosition();
+            // If at target, do not move
+            if (!atTarget)
+            {
+                MoveTowardsPoint();
+            }
+        }
     }
 
-    
+    // Moves player towards the point
+    private void MoveTowardsPoint()
+    {
+        // Look at point
+        this.transform.LookAt(new Vector3(targetPosition.x, this.transform.position.y, targetPosition.z));
+
+        // Change velocity
+        playerVelocity = new Vector3(targetPosition.x, this.transform.position.y, targetPosition.z) - this.transform.position;
+
+        // Move player
+        this.GetComponent<Rigidbody>().AddForce(playerVelocity.normalized * speed);
+    }
 
     // Sets the position target needs to go to
     private void SetTargetPosition()
     {
+        // Get the point where the mouse clicked
         Ray point = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
+        // If it hit something
         if (Physics.Raycast(point, out hit, 1000))
         {
-            targetPosition = hit.point;
+            // If hit player
+            if (hit.transform == this.transform)
+            {
+                atTarget = true;
+            }
+            else
+            {
+                targetPosition = hit.point;
+                atTarget = false;
+            }
         }
     }
 
