5207773d3d006c8a0d1b6fb108633cd05fdb62eb
PsvitaNeedsCat <53197859+PsvitaNeedsCat@users.noreply.github.com>
2019-08-17 13:52:26 +1200

    Door Switches
    - WIP

---
+++ Prototype3/Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour
+{
+    // Public variables
+    public GameObject door1;
+    public GameObject door2;
+
+    // Private variables
+    const float triggerRadius = 2.0f;
+    private bool door1Unlocked = true;
+    private GameObject player;
+
+    private void Awake()
+    {
+        // Set player
+        player = GameObject.Find("Player");
+
+        CheckDoors();
+    }
+
+    private void FixedUpdate()
+    {
+        // Check if player is close enough
+
+    }
+
+    private void CheckDoors()
+    {
+        // Set door status
+        if (door1Unlocked)
+        {
+            door1.GetComponent<Collider>().isTrigger = true;
+            door2.GetComponent<Collider>().isTrigger = false;
+        }
+        else
+        {
+            door2.GetComponent<Collider>().isTrigger = true;
+            door1.GetComponent<Collider>().isTrigger = false;
+        }
+    }
+
+    // When clicked
+    private void OnMouseDown()
+    {
+        // If player is within trigger radius
+        if ((player.transform.position - this.transform.position).magnitude < triggerRadius)
+        {
+            door1Unlocked = !door1Unlocked;
+
+            CheckDoors();
+        }
+    }
+}
