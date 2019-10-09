20e613a28ebae3258c9b96a8baba08aab307eb9d
Samuel Fortune <s.fortune66@gmail.com>
2019-10-09 19:50:49 +1300

    StayAtMidpoint

---
+++ Prototype3/Assets/Scripts/StayAtMidpoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayAtMidpoint : MonoBehaviour
+{
+    /// <summary>  
+    /// This object will stay at the halfway point between Point A and Point B.
+    /// Point A is the Player (if it hasn't been overriden) and Point B is <see cref="target"/>.
+    /// </summary>  
+    [SerializeField] GameObject target;
+
+    /// <summary>  
+    /// Reference to the player. Can be overriden by another GameObject in the editor.
+    /// </summary>  
+    [SerializeField] GameObject playerOverridable;
+
+    private void Awake()
+    {
+        if (!playerOverridable) { playerOverridable = GameObject.Find("Player"); }
+    }
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = target.transform.position;
+        Vector3 playerPosition = playerOverridable.transform.position;
+        Vector3 difference = targetPosition - playerPosition;
+        Vector3 midPoint = playerPosition + difference * 0.5F;
+        this.transform.position = midPoint;
+    }
+}
