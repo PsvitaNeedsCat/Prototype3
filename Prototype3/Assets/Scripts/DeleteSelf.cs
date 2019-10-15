bc9019f661c8ffbbcb6447c855096ce31cfce533
Sam <s.fortune66@gmail.com>
2019-10-16 12:11:05 +1300

    Tut plane prefabs

---
+++ Prototype3/Assets/Scripts/DeleteSelf.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSelf : MonoBehaviour
+{
+    [SerializeField] float timeTillDeletion;
+    float timeSinceSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSinceSpawn = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceSpawn += Time.deltaTime;
+        if (timeSinceSpawn >= timeTillDeletion)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
